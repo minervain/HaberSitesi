1101a553caf3e9a23389aa00fa46081d603130eb
Ali Osman UÇAR <aliosmanucar987@gmail.com>
2023-04-08 14:20:47 +0300

    SonHal
    Sonhali

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NewsapiserviceService } from '../services/newsapiservice.service';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor(private newsApi:NewsapiserviceService) { }
 
   ngOnInit(): void {
   }
 
-}
+  search(searchText:string):void{
+    console.log(searchText)
+
+  }
+  }
+
+
+
